aedd75c1df46f3417e0e4ea3124e6fa3747e9dcb
Lucy Julian <lucyjulian@Lucys-MacBook-Pro.local>
2018-06-13 19:30:42 -0400

    new put route for bookings

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -42,6 +42,19 @@ module.exports = function(app) {
     });
   });
 
+  app.put("/api/:id/book", function(req, res) {
+    db.Booking.update(
+      client_id: req.body.client_id,
+      {
+        where: {
+          barber_id: req.params.id,
+          booking_time: req.body.booking_time
+        }
+      }).then(function(dbBooking) {
+      res.json(dbBooking);
+    });
+  });
+
 };
 
 
