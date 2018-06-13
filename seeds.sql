a9bc966c5ef0acfeaa4826caf891b5c5fdfffc30
Lucy Julian <lucyjulian@Lucys-MacBook-Pro.local>
2018-06-13 18:39:21 -0400

    new seeds file for bookings

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE groove_db;
+
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "9:30-10:00 AM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "10:00-10:30 AM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "10:30-11:00 AM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "11:00-11:30 AM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "11:30-12:00 AM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "12:00-12:30 AM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "12:30-1:00 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "1:00-1:30 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "1:30-2:00 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "2:00-2:30 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "2:30-3:00 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "3:00-3:30 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "3:30-4:00 PM");
+INSERT INTO Bookings (barber_id, booking_time) VALUES (1, "4:00-4:30 PM");
